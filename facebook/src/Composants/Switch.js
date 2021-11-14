36adae05d451a5e2d480c01a65c7a1807206425c
kassim0 <kassim_haddad@yahoo.fr>
2021-11-14 15:37:55 +0100

    .

---
+++ facebook/src/Composants/Switch.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Switch extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Profile</h1>
+            </div>
+        );
+    }
+
+}
+
+export default Switch;
